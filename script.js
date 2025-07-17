a86644caab4e70dd9ab76bc46f0180c644b4e450
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2025-07-17 08:36:41 +0200

    Add coloring
    Added being able to color squares by hovering over them

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const containerStyle = window.getComputedStyle(container)
 function formGrid (size) {
     for (i = 0; i < size*size; i++) {
         const grid = document.createElement('div')
+        grid.classList.add("grid")
         container.appendChild(grid)
         grid.style.height = (parseInt(containerStyle.getPropertyValue("height")) / size) + "px"
         grid.style.width = (parseInt(containerStyle.getPropertyValue("width")) / size) + "px"
         grid.style.border = "1px solid gray"
         grid.style.boxSizing = "border-box"
     }
+
 }
 
 formGrid(16)
+
+let gridSquare = document.querySelectorAll(".grid")
+gridSquare.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = "black"
+    })
+})
